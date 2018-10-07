a5cfbd096c9c8fbbc011cdbdcaedbcc53810015b
Gerasim Troeglazov <3dEyes@gmail.com>
2018-10-07 12:50:05 +1000

    Change default game list source

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-#define GAMES_UPDATE_URL "http://instead.sf.net/pool/game_list.xml"
+#define GAMES_UPDATE_URL "http://instead-games.ru/xml.php"
 #define SW_UPDATE_URL "http://instead.sf.net/pool/update_list.xml"
 #define LAUNCHER_VERSION "0.7.0"
 
